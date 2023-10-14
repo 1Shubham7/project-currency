753295d720c4e4539887d3101a137e3333265bc2
Shubham Singh <shubhammahar1306@gmail.com>
2023-10-14 07:06:29 +0530

    add: added basic file structure, working on order.go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/1shubham7/basic-unit-test
+
+go 1.20
+
+require github.com/Rhymond/go-money v1.0.10 // indirect
---
+++ internal/order/order.go
@@ -0,0 +1,31 @@
+package order
+
+import (
+	"github.com/Rhymond/go-money"
+	"fmt"
+)
+
+type Order struct {
+	ID                string
+	CurrencyAlphaCode string
+	Items             []Item
+}
+
+type Item struct {
+	ID        string
+	Quantity  uint
+	UnitPrice *money.Money
+}
+
+func (o Order) ComuteTotal() (*money.Money, error) {
+	amount := money.New(0, o.CurrencyAlphaCode)
+	for _, item := range o.Items {
+		var err error
+		amount, err = amount.Add(item.UnitPrice)
+		if err!= nil {
+			return nil, fmt.Errorf("not adding item elements, error: %w", err)
+		}
+	}
+
+	return amount, nil
+}
